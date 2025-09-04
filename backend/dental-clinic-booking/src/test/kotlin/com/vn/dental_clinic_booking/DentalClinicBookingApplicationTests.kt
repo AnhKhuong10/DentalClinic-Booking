5a080bc2fcea6432dc88f36f08a5d4a08c115319
Anh Khuong <anhkhuong989@gmail.com>
2025-09-04 17:33:06 +0700

    Add backend project (Spring Boot init)

---
+++ backend/dental-clinic-booking/src/test/kotlin/com/vn/dental_clinic_booking/DentalClinicBookingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.vn.dental_clinic_booking
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DentalClinicBookingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
